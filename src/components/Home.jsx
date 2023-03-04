2992d85d328ec90831d81d5c76f8e7b37e0db087
unknown <armandoriv98@hotmail.com>
2023-03-04 17:48:47 -0600

    hue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,29 +21,48 @@ const Home = () => {
             </div>
         </section>
         <section className='submain container-fluid'>
-            <p>En diseño dental contamos con:</p>
+            <p data-aos="fade-down" data-aos-once="true" data-aos-duration="1000">En diseño dental contamos con:</p>
             <div className='row'>
-                <div className='subDetail col-lg-3 col-md-6'>
+                <div className='subDetail col-lg-3 col-md-6' data-aos="fade-up" data-aos-once="true" data-aos-duration="1250">
                     <img className='iconMain' src="../img/doctor.png" alt="" />
                     <p className='subDetailTxt'>Doctores Amables</p>
 
                 </div>
-                <div className='subDetail col-lg-3 col-md-6'>
+                <div className='subDetail col-lg-3 col-md-6' data-aos="fade-up" data-aos-once="true" data-aos-duration="1750">
                     <img className='iconMain' src="../img/equipo.png" alt="" />
                     <p className='subDetailTxt'>Equipos de última tecnología</p>
 
                 </div>
-                <div className='subDetail col-lg-3 col-md-6'>
+                <div className='subDetail col-lg-3 col-md-6' data-aos="fade-up" data-aos-once="true" data-aos-duration="2250">
                     <img className='iconMain' src="../img/silla.png" alt="" />
                     <p className='subDetailTxt'>Comodas instalaciones</p>
 
                 </div>
-                <div className='subDetail col-lg-3 col-md-6'>
+                <div className='subDetail col-lg-3 col-md-6' data-aos="fade-up" data-aos-once="true" data-aos-duration="2750">
                     <img className='iconMain' src="../img/garantizado.png" alt="" />
                     <p className='subDetailTxt'>Satisfacción garantizada</p>
 
                 </div>
             </div>
+        </section>
+        <section className='especialidades containter-fluid row'>
+            <div className='col-lg-4 col-md-12'>
+                <h3 className='mb-4' data-aos="fade-right" data-aos-once="true" data-aos-duration="1000">Somos una clinica especializada, que cuenta con las especialidades de:</h3>
+                <p className='list'data-aos="fade-right" data-aos-once="true" data-aos-duration="1000">Odontología</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1200">Dentista</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1300">Ortodoncia</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1400">Ortopedia Maxilar</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1500">Prótesis Dental</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1600">Implantología Dental</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1700">Estetica Bucal</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1800">Endodoncia</p>
+                <p className='list' data-aos="fade-right" data-aos-once="true" data-aos-duration="1900">Odontología</p>
+            </div>
+            <div className='col-lg-8 col-md-12'>
+                <img className='especialidadImg' src="../img/analisis.jpg" alt="" />
+
+            </div>
+
         </section>
 
             
